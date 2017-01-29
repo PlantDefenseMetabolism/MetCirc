5a9f84a5ab6a010b89e091df6ac5ca03edb0ac9f
tnaake <thomasnaake@gmx.de>
2017-01-29 10:27:49 +0100

    update unit test for binning, use tol=0, and negative tol

--- inst/unitTests/test_binning.R
+++ inst/unitTests/test_binning.R
@@ -1,4 +1,5 @@
-
+## load data sets
+data("sd02_deconvoluted", package = "MetCirc")
 
 ## START unit test getBegEndIndMSP
 testMSP <- convert2MSP(sd02_deconvoluted, split = " _ ", 
@@ -20,10 +21,15 @@ test_getBegEndIndMSP <- function() {
 compartment <- c(rep("a", 90), rep("b", 90), rep("c", 90), rep("d", 90))
 ## create binnedMSPs
 binnedMSP001 <- binning(testMSP, 0.01, group = compartment, method = "mean")
+binnedMSP0 <- binning(testMSP, 0, group = compartment, method = "mean")
 test_binning <- function() {
     checkTrue(is.matrix(binnedMSP001))
     checkTrue(is.numeric(binnedMSP001))
     checkEquals(dim(binnedMSP001), c(360, 764)) 
     checkException(binning(finalMSP, 1, compartment[1:7]))
+    checkEquals(dim(binnedMSP0)[2], length(unique(testMSPmsp[,1])) - 2 ) 
+    ## check if all unique fragments are used 
+    ## (- 2 because of "Num Peaks: " and " ")
+    checkException(binning(finalMSP, -0.1))
 }
 ## END unit test binning
